70d3e1fa817edaf86c808789d7e3652cfc0110bc
Ola Kaznowska <118020449+Ola-Kaznowska@users.noreply.github.com>
2024-08-01 13:28:00 +0200

    Add files via upload

---
+++ Library Version AI.py
@@ -0,0 +1,5 @@
+import numpy as np
+import tensorflow as tf
+
+print(np.__version__)
+print(tf.__version__)
---
+++ Neural Network AI.py
@@ -0,0 +1,25 @@
+import numpy as np   
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import TensorBoard
+
+name = "linear_function"
+TensorBoard = TensorBoard(log_dir=f"logs/{name}")
+
+
+layer = Dense(units=1, input_shape=[1])
+model = Sequential([layer])
+model.compile(optimizer="sgd", loss="mean_squared_error", metrics=["accuracy"])
+
+
+xs = np.array([-2, -1, 0, 1, 2, 3])
+xy = np.array([-13, -8, 3, 2, 7, 12])
+
+
+
+model.fit(xs, xy, epochs=100, callbacks=[TensorBoard])
+
+print(model.predict(np.arry([10], dtype=int)))
+print(layer.get_weights())
+
+
